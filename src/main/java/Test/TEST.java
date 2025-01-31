6b41441698017d97aba87ccf1d6e90f2544cad06
AntonP17 <pokrovskii.anton@mail.ru>
2025-02-01 00:32:31 +0300

    логирование log4j slf4j
    разобрался с логированием , можно во время выполнения программы следаить за всем происходящими выводит логи проверки в консоль в ависимости от события

--- src/main/java/Test/TEST.java
+++ src/main/java/Test/TEST.java
@@ -1,14 +1,25 @@
 package Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Date;
+
 
 public class TEST {
 
-    public static void main(String[] args) {
+    // Создаём логгер на данный класс
+    private static final Logger logger = LoggerFactory.getLogger(TEST.class);
 
-        System.out.println("sdas");
+    public static void main(String[] args) {
+        // Пример логирования
+        logger.trace("выводится все сообщения");
+        logger.info("Приложение запущено! прсотые сообщение");
+        logger.debug("Это сообщение уровня DEBUG.");
+        logger.warn("Это сообщение уровня WARN.");
+        logger.error("Это сообщение уровня ERROR.");
+
+
     }
 }
