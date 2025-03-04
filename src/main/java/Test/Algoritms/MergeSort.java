712422bf6ec5056dcc80a0316b0b572e30e882ed
AntonP17 <pokrovskii.anton@mail.ru>
2025-03-04 11:26:57 +0300

    Create MergeSort.java
    learn algoritm

---
+++ src/main/java/Test/Algoritms/MergeSort.java
@@ -0,0 +1,62 @@
+package Test.Algoritms;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,20,1,33,543,22,8};
+        mergeSort(arr, 0, arr.length - 1);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid); // левая часть
+            mergeSort(arr, mid + 1, right); // правая часть
+            // делим до тех пор пока не останется несколько массивов только с 1 элементом
+            merge(arr, left, mid, right); // слияние обеих
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            leftArr[i] = arr[left + i];
+        }
+        for (int i = 0; i < n2; i++) {
+            rightArr[i] = arr[mid + 1 + i];
+        }
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+}
