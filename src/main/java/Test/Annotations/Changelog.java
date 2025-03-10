840adb4d5e9c7483905101b1881ad1982cc41cb7
AntonP17 <pokrovskii.anton@mail.ru>
2025-03-10 21:27:24 +0300

    add annotations base

--- src/main/java/Test/Annotations/Changelog.java
+++ src/main/java/Test/Annotations/Changelog.java
@@ -1,9 +1,12 @@
 package Test.Annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(value = ElementType.TYPE)
 public @interface Changelog {
     //напиши свой код
     Revision[]value() default {};
